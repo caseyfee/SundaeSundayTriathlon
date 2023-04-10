7bcedf2dba779191a7b4b4d7c6eb160864504743
caseyfee <caseyfee3@gmail.com>
2023-04-09 17:58:04 -0700

    voting button and routes set up

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -79,4 +79,14 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/voting', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/voting');
+    return;
+  }
+
+  res.render('voting');
+});
+
 module.exports = router;

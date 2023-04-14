0d754a52780747afdbbbc02ef39e3301b26bc9bc
Nathan Stepper <nstepper@outlook.com>
2023-04-13 20:17:56 -0700

    idk

--- Main/config/connection.js
+++ Main/config/connection.js
@@ -6,7 +6,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
+  /*sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
@@ -15,6 +15,17 @@ if (process.env.JAWSDB_URL) {
       dialect: 'mysql',
       port: 3306
     }
+    */
+
+    sequelize = new Sequelize(
+      'iceCreamTri_db',
+      'root',
+      'password',
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+      }
   );
 }
 
--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -1,3 +1,4 @@
 DROP DATABASE IF EXISTS iceCreamTri_db;
 CREATE DATABASE iceCreamTri_db;
 USE iceCreamTri_db;
+db 
